ab0dc4e9255e5a43fceb0b1cc544922487cfe3f7
asia chicon <chiconba@my.erau.edu>
2020-08-31 23:41:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Inheritance
+{
+        class Baby
+        {
+            public static void Cry()
+        {
+            if(ConsoleKey.Enter = 1)
+            Console.WriteLine("Waaaaaaa");
+        }
+            public static void NapTime()
+        {
+            Console.WriteLine("Zzzzzzz");
+        }
+
+        }
+
+
+
+
+        class Program
+    { 
+        static void Main(string[] args)
+        {
+            Baby.Cry();
+            Baby.NapTime();
+
+        }
+    }
+}
